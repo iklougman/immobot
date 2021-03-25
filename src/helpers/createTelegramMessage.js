0a70ae37a3c1b9c03f61af2f069193e270933da1
Igor Klougman <igor.klougman@yandex.com>
2021-03-25 14:55:03 +0100

    debug production envs

--- src/helpers/createTelegramMessage.js
+++ src/helpers/createTelegramMessage.js
@@ -12,17 +12,10 @@ export const createTelegramMessage = (data = []) => {
 }
 
 export const findDiffs = (findings = []) => {
+    console.log(process.env)
     const rawData = fs.readFileSync(path.join(__dirname, '../data', `/data_${process.env.TELEGRAM_USER_ID}.json`)) || []
     const storedFindings = JSON.parse(rawData) || []
 
-    if (!storedFindings.length) return findings
-
-    const [firstStored = {}] = storedFindings;
-    const [first = {}] = findings;
-    if (JSON.stringify(firstStored) === JSON.stringify(first)) {
-        console.log('0 differences found')
-        return []
-    }
     const diff = findings.filter((finding, index) => {
         if (!!storedFindings[index] && finding.priceNum < 450000 && finding.priceNum > 100000) {
             return finding.link !== storedFindings[index].link
