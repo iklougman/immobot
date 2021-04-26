651a92b3a43a719faac5be937c65b8dfe4af6857
Igor Klougman <igor.klougman@yandex.com>
2021-04-26 23:42:10 +0200

    forgotten scheduler

--- src/index.ts
+++ src/index.ts
@@ -32,24 +32,24 @@ async function start() {
     telegramBot.start((ctx) => ctx.reply('Herzlich wilkommen zu Immobot'))
     const parser = new Parser()
 
-    // cron.schedule('*/2 * * * *', async () => {
+    cron.schedule('*/2 * * * *', async () => {
 
-    logger.info('parsing started')
+        logger.info('parsing started')
 
-    const findings = await parser.parse(settings, logger)
+        const findings = await parser.parse(settings, logger)
 
-    const matches: IFinding[] = findMatches(findings)
+        const matches: IFinding[] = findMatches(findings)
 
-    logger.info(`${matches.length} matches found`)
+        logger.info(`${matches.length} matches found`)
 
-    await Promise.all(matches.map(async (match: IFinding) => {
-        //@ts-ignore
-        await telegramBot.telegram.sendPhoto(process.env.TELEGRAM_USER_ID, `${match.picture}`,
-            {
-                caption: constructSingleMessage(match), parse_mode: 'HTML', disable_web_page_preview: false
-            })
-    })).catch((e) => logger.error('Telegram message sending results to ', e));
-    // });
+        await Promise.all(matches.map(async (match: IFinding) => {
+            //@ts-ignore
+            await telegramBot.telegram.sendPhoto(process.env.TELEGRAM_USER_ID, `${match.picture}`,
+                {
+                    caption: constructSingleMessage(match), parse_mode: 'HTML', disable_web_page_preview: false
+                })
+        })).catch((e) => logger.error('Telegram message sending results to ', e));
+    });
 }
 
 start()
